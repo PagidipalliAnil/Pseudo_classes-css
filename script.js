7861ddbb8e0905a35a21dbc8a7fc6e3cb05e3b13
PagidipalliAnil <pagidipallianil3001@gmail.com>
2024-08-28 21:02:40 +0530

    login commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('conatainer');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', ()=>{
+    container.classList.add("active");
+});
+loginBtn.addEventListener('click', ()=>{
+    container.classList.remove("active");
+});
